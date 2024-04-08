a40180b9117a8e4a4fb03c1771be5e1eea112213
“Emeka <ngwe7236@gmail.com>
2024-04-08 02:48:37 +0200

    update server side

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+// const cors = require('cors');
+// const app = require("./app");
+// app.use(cors());
+// const PORT = process.env.PORT || 5005;
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on http://localhost:${PORT}`);
+// });
+
+
+
+
+const cors = require('cors');
 const app = require("./app");
+
+// Enable CORS for all routes
+app.use(cors());
 
 const PORT = process.env.PORT || 5005;
 
