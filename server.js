426c5d473e6d418e770b791bea45b4d5260419c8
“Emeka <ngwe7236@gmail.com>
2024-04-10 02:59:59 +0200

    update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 
 const app = require("./app");
+const cors = require("cors")
 
+
+app.use(cors())
 
 const PORT = process.env.PORT || 5005;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
