af14115d8ec83977f86009bcf09bb126486646e9
“Emeka <ngwe7236@gmail.com>
2024-04-10 23:45:26 +0200

    .

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = (app) => {
     cors({
       origin: [FRONTEND_URL],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      // allowedHeaders: ['Content-Type', 'Authorization'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
       // preflightContinue: false,
       // optionsSuccessStatus: 204
