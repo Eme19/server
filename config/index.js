6a170c51e4b4d70d712dd2d548a698de00dd7861
“Emeka <ngwe7236@gmail.com>
2024-04-10 18:20:22 +0200

    .

--- config/index.js
+++ config/index.js
@@ -27,6 +27,9 @@ module.exports = (app) => {
       origin: [FRONTEND_URL],
       methods: ['GET', 'POST', 'PUT', 'DELETE', ' OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+      preflightContinue: false,
+      optionsSuccessStatus: 204
     })
   );
 
