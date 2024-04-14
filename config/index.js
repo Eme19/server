fa591d795a27be43a2c9c12da3f0bbf1380e768e
“Emeka <ngwe7236@gmail.com>
2024-04-14 21:49:11 +0200

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN || "https://u-play.vercel.app"
+// const FRONTEND_URL = process.env.ORIGIN || "https://u-play.vercel.app"
 
 
 module.exports = (app) => {
@@ -20,12 +20,10 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin:  "https://u-play.vercel.app",
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
-      preflightContinue: false, 
-      optionsSuccessStatus: 204 
     })
   );
   
--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 
 const app = require("./app");
-const cors = require("cors")
-
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
 
 
 const PORT = process.env.PORT || 5005;
