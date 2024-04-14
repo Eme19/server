40d1aa141cc090e8fbe954977976691d48f2f6b5
“Emeka <ngwe7236@gmail.com>
2024-04-14 20:47:58 +0200

    .

--- app.js
+++ app.js
@@ -8,13 +8,6 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  next();
-});
-
-
 
 require("./config")(app);
 
