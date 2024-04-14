c76af0f10db690cba7159ae68e4911bab4b70072
“Emeka <ngwe7236@gmail.com>
2024-04-14 06:44:04 +0200

    update

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
 require("dotenv").config();
 
-// ℹ️ Connects to the database
+
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+//   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+//   next();
+// });
+
+
 app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader('Access-Control-Allow-Origin', 'https://u-play.vercel.app');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+
+
 require("./config")(app);
 
-// 👇 Start handling routes here
+
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
