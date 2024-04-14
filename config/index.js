cbb7142bc5999433a3d8ec1478121494af4ef63b
“Emeka <ngwe7236@gmail.com>
2024-04-14 17:52:06 +0200

    .

--- config/index.js
+++ config/index.js
@@ -24,8 +24,8 @@ module.exports = (app) => {
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true,
-      preflightContinue: false, 
-      optionsSuccessStatus: 204 
+      // preflightContinue: false, 
+      // optionsSuccessStatus: 204 
     })
   );
   
