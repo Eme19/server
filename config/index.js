015c52fd07bdccd6bedf41bd6e42c7b50b34ad52
“Emeka <ngwe7236@gmail.com>
2024-04-24 23:04:41 +0200

    update

--- config/index.js
+++ config/index.js
@@ -41,43 +41,43 @@
 
 
 
-// // config.js
+// config.js
 
-// const express = require("express");
-// const logger = require("morgan");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-// const FRONTEND_URL = process.env.ORIGIN || "https://u-play.vercel.app";
+const FRONTEND_URL = process.env.ORIGIN || "https://u-play.vercel.app";
 
-// // Define allowCors middleware
-// const allowCors = fn => async (req, res) => {
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
-//   // You can also use the following line to allow requests from any origin
-//   // res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//   );
-//   if (req.method === 'OPTIONS') {
-//     res.status(200).end();
-//     return;
-//   }
-//   return await fn(req, res);
-// };
+// Define allowCors middleware
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
+  // You can also use the following line to allow requests from any origin
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-// module.exports = (app) => {
-//   app.set("trust proxy", 1);
+module.exports = (app) => {
+  app.set("trust proxy", 1);
 
-//   // Apply allowCors middleware to all routes
-//   app.use(allowCors);
+  // Apply allowCors middleware to all routes
+  app.use(allowCors);
 
-//   app.use(logger("dev"));
-//   app.use(express.json());
-//   app.use(express.urlencoded({ extended: false }));
-//   app.use(cookieParser());
-// };
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+};
 
 
